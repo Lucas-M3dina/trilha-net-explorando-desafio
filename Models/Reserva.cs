b4b68eb3a02cf23d6902de3d63217ce53d396275
Lucas-M3dina <lucas.m3dina@gmail.com>
2024-08-15 22:50:53 -0300

    feat: Resolução do desafio

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -15,16 +15,18 @@ namespace DesafioProjetoHospedagem.Models
 
         public void CadastrarHospedes(List<Pessoa> hospedes)
         {
+            if (hospedes == null) return;
             // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
-            // *IMPLEMENTE AQUI*
-            if (true)
+            // FEITO
+            if (Suite.Capacidade >= hospedes.Count())
             {
                 Hospedes = hospedes;
             }
             else
             {
                 // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
-                // *IMPLEMENTE AQUI*
+                // FEITO
+                throw new Exception("Capacidade menor que o número de hóspedes recebido");
             }
         }
 
@@ -36,22 +38,22 @@ namespace DesafioProjetoHospedagem.Models
         public int ObterQuantidadeHospedes()
         {
             // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
-            // *IMPLEMENTE AQUI*
-            return 0;
+            // FEITO
+            return Hospedes.Count();
         }
 
         public decimal CalcularValorDiaria()
         {
             // TODO: Retorna o valor da diária
             // Cálculo: DiasReservados X Suite.ValorDiaria
-            // *IMPLEMENTE AQUI*
-            decimal valor = 0;
+            // FEITO
+            decimal valor = Suite.ValorDiaria * DiasReservados;
 
             // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
-            // *IMPLEMENTE AQUI*
-            if (true)
+            // FEITO
+            if (DiasReservados >= 10)
             {
-                valor = 0;
+                valor -= valor * 0.10M;
             }
 
             return valor;
